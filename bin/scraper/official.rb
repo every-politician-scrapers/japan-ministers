4f76dcf49db153b06708d29a85281ece3720112f
Tony Bowden <tony@tmtm.com>
2021-08-27 12:53:20 +0300

    Also fetch some other positions
    Each member has multiple positions, but initially we're only taking the
    first one. Some of the others already exist in Wikidata, though, so
    allow those to be collected as well.
    
    Later we should include _all_ the positions and ensure that everyone has
    all of them set in Wikidata.

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -6,12 +6,28 @@ require 'pry'
 
 class MemberList
   class Member
+    # everyone holds multiple positions, but we're only taking the first
+    # one for now, unless they're also in this list
+    # TODO: ensure Wikidata knows *all* of the positions
+    OTHER_POSITIONS = [
+      'Minister of the Environment',
+      'Minister of State for Regulatory Reform'
+    ].freeze
+
     def name
       name_and_position[0]
     end
 
     def position
+      [first_position] + other_positions
+    end
+
+    def first_position
       name_and_position[1]
+    end
+
+    def other_positions
+      lines.drop(1) & OTHER_POSITIONS
     end
 
     private
@@ -21,7 +37,7 @@ class MemberList
     end
 
     def lines
-      noko.xpath('text()').map(&:text)
+      noko.xpath('text()').map(&:text).map(&:tidy)
     end
   end
 
