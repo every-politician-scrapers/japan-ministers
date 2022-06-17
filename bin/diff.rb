23835b8b463d03a8270bab3eb6f0c80e70e483d3
Tony Bowden <tony@tmtm.com>
2022-06-17 13:10:38 +0300

    update to DecoratedComparison

--- bin/diff.rb
+++ bin/diff.rb
@@ -5,6 +5,6 @@ require 'every_politician_scraper/comparison'
 
 SKIP = ['State Minister', 'Parliamentary Vice-Minister'].freeze
 
-diff = EveryPoliticianScraper::NulllessComparison.new('data/wikidata.csv', 'data/official.csv').diff
+diff = EveryPoliticianScraper::DecoratedComparison.new('data/wikidata.csv', 'data/official.csv').diff
                                          .reject { |row| SKIP.include? row.last }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
